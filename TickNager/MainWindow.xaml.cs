d9acd928c7f3a9f0ad1aa07b08d89846006053c2
Royer <rogerpaul197@gmail.com>
2026-02-14 23:36:28 +0100

    Proyecto avanzado un 30%

---
+++ TickNager/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Windows;
+
+namespace TickNager
+{
+    public partial class MainWindow : Window
+    {
+        string nombre;
+        string contrasena;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSesion_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            nombre = txtUsuario.Text;
+            contrasena = txtContrasena.Password;
+
+            ///Este es el usuario por defecto al iniciar el programa por primera vez
+            ///que normalmente será el administrador.
+            ///Después el propio administrador se encargará de crear las demás cuentas
+            if (nombre == "admin" && contrasena == "1234")
+            {
+                MessageBox.Show("Se ha iniciado sesión correctamente");
+                Principal principal = new Principal();
+                principal.Show();
+                this.Close();
+            }
+            else
+            {
+                txtError.Text = "El usuario o contraseña es incorrecto";
+            }
+        }
+    }
+}
